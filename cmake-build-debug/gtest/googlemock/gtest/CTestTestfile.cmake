50a0753955d95796a32536d974feadecc116ef1a
wuzida <501215134@qq.com>
2020-03-25 13:24:28 +0800

    delete useless files

--- cmake-build-debug/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/wuzida/catkin_ws/src/MSF_developed/cmake-build-debug/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/setup_custom_pythonpath.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
-
-PYTHONPATH=/home/wuzida/catkin_ws/src/MSF_developed/cmake-build-debug/devel/lib/python2.7/dist-packages:$PYTHONPATH
-exec "$@"
